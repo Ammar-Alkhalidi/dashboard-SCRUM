c979eaf0c9e8ee43ce136c2497c990c783e7e334
Felix Kalske <felix.kalske@dci-student.org>
2025-02-13 15:50:25 +0100

    feat: create Products structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Dashboard from "./Views/Dasboard/Dashboard";
+import Products from "./Views/Products/Products";
 
 const App = () => {
   return (
     <main>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/products" element={<div>Products</div>} />
+        <Route path="/products" element={<Products/>} />
         <Route path="/order-list" element={<div>Order List</div>} />
         <Route path="/product-stock" element={<div>Product Stock</div>} />
         <Route path="/to-do" element={<div>To-Do</div>} />
---
+++ client/src/Views/Products/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>
+      Hallo Welt
+    </div>
+  )
+}
+
+export default Products
