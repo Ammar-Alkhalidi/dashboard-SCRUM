78fe7842c3fc8a5577632532e05aa069e6b1f11d
yousifpa98 <yousif.paulus@dci-student.org>
2025-02-18 16:11:37 +0100

    feat: general task styling

--- client/src/Views/ToDo/Components/Task.jsx
+++ client/src/Views/ToDo/Components/Task.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import "./css/Task.css"
+import React from "react";
+import "./css/Task.css";
+import XMark from "../../../assets/icons/xmark.svg?react";
+import StarSolid from "../../../assets/icons/star-solid.svg?react";
+import StarRegular from "../../../assets/icons/star-regular.svg?react";
 
-const Task = ({task}) => {
+const Task = ({ task }) => {
   return (
-    <div>Task</div>
-  )
-}
+    <div className="task-card container ">
+      <div className="task-title">
+        <input type="checkbox" name="isDone" id="isDone" />
+        <h3>{task.name}</h3>
+      </div>
+      <div className="controls">
+        {task.isFav ? (
+          <StarSolid className="task-icon star-solid" />
+        ) : (
+          <StarRegular className="task-icon star-reg" />
+        )}
 
-export default Task
+        <button className="xmark-btn">
+          <XMark className="task-icon xmark" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Task;
