cb4a1200cd3c797826f79d9a730640df1d9dbd5b
Ammar <ammar.alkhalidi@dci-student.org>
2025-02-24 16:00:23 +0100

    fix:fix code review by Yousif

--- client/src/Views/Team/Components/TeamMember.jsx
+++ client/src/Views/Team/Components/TeamMember.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import "./css/TeamMember.css";
 
-const TeamMember = ({ member }) => {
+const TeamMember = ({ name, role, email, img }) => {
   return (
     <div className="team-member">
-      <img src={member.img} alt={member.name} />
-      <h3>{member.name}</h3>
-      <p>{member.role}</p>
-      <a href={`mailto:${member.email}`}>{member.email}</a>
+      <img className="team-img" src={img} alt={name} />
+      <h3>{name}</h3>
+      <p>{role}</p>
+      <a href={`mailto:${email}`}>{email}</a>
     </div>
   );
 };
 
 export default TeamMember;
-// Compare this snippet from client/src/Views/Team/Components/TeamMember.jsx:
-// import React from "react";
-// import "./TeamMember.css";
-//
-// const TeamMember = ({ member }) =>
-//   <div className="team-member"></div>
-//     <img src={member.img} alt={member.name} />
--- client/src/Views/Team/Team.jsx
+++ client/src/Views/Team/Team.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import './Team.css'
-import { useTeam } from '../../Contexts/TeamProvider'
-import TeamMember from './Components/TeamMember'
-
+import React from "react";
+import "./Team.css";
+import { useTeam } from "../../Contexts/TeamProvider";
+import TeamMember from "./Components/TeamMember";
 
 const Team = () => {
   const { team } = useTeam();
 
   return (
-    <section className="team-container">
-      <div className="header">
+    <section className="team">
+      <div className="header-team">
         <h2>Team</h2>
         <button className="add-btn">Add New Member</button>
       </div>
       <div className="team-grid">
-        {team.map((member) => (
-          <TeamMember key={member.id} member={member} />
+        {team.map((m) => (
+          <TeamMember
+            key={m.id}
+            name={m.name}
+            role={m.role}
+            email={m.email}
+            img={m.img}
+          />
         ))}
       </div>
     </section>
@@ -23,5 +28,3 @@ const Team = () => {
 };
 
 export default Team;
-
-
